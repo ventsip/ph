5f837830a64dd6d762abc9922cac5a4c0f1b8600
ventsip <ventsip@gmail.com>
2019-11-22 00:22:54 +0200

    - term_process recognizes SIGTERM

--- test_process/main.go
+++ test_process/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	log.Println("Starting")
+
+	term := make(chan os.Signal, 2)
+	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+
+	timer := time.NewTimer(10 * time.Second)
+
 	for {
-		log.Println("sleeping")
-		time.Sleep(time.Second * 3)
+		select {
+		case <-term:
+			log.Println("SIGTERM received. Exiting")
+			return
+		case <-timer.C:
+			log.Println("Sleeping")
+		}
 	}
 }
