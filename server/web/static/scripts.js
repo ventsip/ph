108fe31ed2ff5cb186e7364da42e6e9bfbb3a815
8-10 <ventsip@gmail.com>
2019-12-29 13:04:00 +0200

    hasing process names to eliminate space in element classes

--- server/web/static/scripts.js
+++ server/web/static/scripts.js
@@ -48,14 +48,25 @@ function genLimits(limits) {
     return t;
 }
 
+function hashCode(str) {
+    let hash = 0;
+    if (str.length == 0) return hash;
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    return hash;
+}
+
 function registerHover(e, name) {
     const h = "w3-blue-grey";
 
-    let cn = "ph-" + name;
-    let s = '.' + $.escapeSelector(cn)
+    let cn = "ph-" + hashCode(name);
+    let s = '.' + $.escapeSelector(cn);
     e.addClass(cn).hover(
-        () => { $(s).addClass(h) },
-        () => { $(s).removeClass(h) }
+        () => { $(s).addClass(h); },
+        () => { $(s).removeClass(h); }
     );
 
     return e;
