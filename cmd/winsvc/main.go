1257d676c4afb8d271ab096c4c57beb52eed0f09
8-10 <ventsip@gmail.com>
2021-11-07 21:57:43 +0200

    rewording comments

--- cmd/winsvc/main.go
+++ cmd/winsvc/main.go
@@ -1,3 +1,4 @@
+//go:build windows
 // +build windows
 
 package main
@@ -14,7 +15,7 @@ import (
 const (
 	svcName        = "ph"
 	svcDisplayName = "Process Hunter"
-	svcDescription = "Enforces daily time limit to processes. See https://bitbucket.org/ventsip/ph/src/master/"
+	svcDescription = "Enforces time limit to processes. See https://bitbucket.org/ventsip/ph/src/master/"
 )
 
 var version = "undefined"
